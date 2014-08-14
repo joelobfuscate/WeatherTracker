7b6078fd3a03c8938e72122340c48926604b8ff5
Joel Frederico <joelfred@slac.stanford.edu>
2014-08-13 20:34:34 -0700

    One to record, one to display

--- WeatherPlotter.py
+++ WeatherPlotter.py
@@ -5,20 +5,26 @@ import urllib2
 import sqlite3
 import time
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import numpy as np
 
-conn = sqlite3.connect('test.db')
-c=conn.cursor()
-c.execute('SELECT * FROM Temp')
-data=c.fetchall()
-conn.close()
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
 
-data=np.array(data)
-
-for row in data:
-	print 'Time: {},\tTemp: {}'.format(row[0],row[1])
-
-plt.plot(data[:,0],data[:,1],'o-')
-
-
-plt.show()
+while True:
+	conn = sqlite3.connect('test.db')
+	c=conn.cursor()
+	c.execute('SELECT * FROM Temp')
+	data=c.fetchall()
+	conn.close()
+	
+	data=np.array(data)
+	
+	# for row in data:
+	#         print 'Time: {},\tTemp: {}'.format(row[0],row[1])
+	
+	ax.clear()
+	ax.plot(data[:,0],data[:,1],'o-')
+	
+	plt.pause(180)
+	# plt.show()
--- WeatherTracker.py
+++ WeatherTracker.py
@@ -6,24 +6,32 @@ import sqlite3
 import time
 
 # Get temperature
+while True:
 
-f=urllib2.urlopen('http://api.wunderground.com/api/01afa3ace37e6ce4/conditions/q/CA/Mountain_View.json')
-json_string = f.read()
-parsed_json = json.loads(json_string)
-location = 'Mountain View'
-temp_f = parsed_json['current_observation']['temp_f']
-print 'Current temperature in {} is {}'.format(location,temp_f)
-f.close()
+	f=urllib2.urlopen('http://api.wunderground.com/api/01afa3ace37e6ce4/conditions/q/CA/Mountain_View.json')
+	json_string = f.read()
+	parsed_json = json.loads(json_string)
+	location = 'Mountain View'
+	try:
+		temp_f = parsed_json['current_observation']['temp_f']
 
-conn = sqlite3.connect('test.db')
-c=conn.cursor()
-try:
-    c.execute('CREATE TABLE Temp(time REAL,temp_f REAL)')
-except:
-    pass
-data=(time.time(),temp_f)
-c.execute('INSERT INTO Temp VALUES(?,?)',data)
+	except:
+		print parsed_json['response']
+		continue
+	print 'Current temperature in {} is {}'.format(location,temp_f)
+	f.close()
+	
+	conn = sqlite3.connect('test.db')
+	c=conn.cursor()
+	try:
+		c.execute('CREATE TABLE Temp(time REAL,temp_f REAL)')
+	except:
+		pass
+	data=(time.time(),temp_f)
+	c.execute('INSERT INTO Temp VALUES(?,?)',data)
+	
+	conn.commit()
+	conn.close()
 
-conn.commit()
-conn.close()
-
+	time.sleep(180)
+	
