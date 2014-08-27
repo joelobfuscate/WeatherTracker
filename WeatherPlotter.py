067e3d909bf58cf1e34e82c439e26a3420045ff8
Joel Frederico <joelfrederico+obfuscate@gmail.com>
2014-08-27 06:49:10 +0000

    Updates to dev branch

--- WeatherPlotter.py
+++ WeatherPlotter.py
@@ -7,24 +7,68 @@ import time
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
+import dropbox as dbox
+import webbrowser
+
+# SQL Database
+dbname = 'test.db'
+
+# Dropbox app keys/secret
+app_key = 'o4ewwn9y97bnjk4'
+app_secret = 'xt3f58jsdpdvlv4'
+access_type = 'app_folder'
+
+# Try to get access_token from SQL
+conn = sqlite3.connect(dbname)
+c = conn.cursor()
+try:
+	c.execute('SELECT * FROM Dropbox')
+	data=c.fetchall()
+	access_token = data
+# Get access token from Dropbox
+except:
+	sess = dbox.session.DropboxSession(app_key,app_secret,access_type)
+	request_token = sess.obtain_request_token()
+	url = sess.build_authorize_url(request_token)
+	webbrowser.
+
+code = raw_input('Enter auth code here: ').strip()
+access_token, user_id = flow.finish(code)
+print access_token
+print user_id
+client = dbox.client.DropboxClient(access_token)
+print 'linked account: {}'.format(client.account_info())
 
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
+sec_per_day = 60.*60.*24.
 
 while True:
-	conn = sqlite3.connect('test.db')
-	c=conn.cursor()
-	c.execute('SELECT * FROM Temp')
-	data=c.fetchall()
-	conn.close()
-	
-	data=np.array(data)
-	
-	# for row in data:
-	#         print 'Time: {},\tTemp: {}'.format(row[0],row[1])
-	
-	ax.clear()
-	ax.plot(data[:,0],data[:,1],'o-',data[:,0],data[:,2],'o-')
-	
-	plt.pause(180)
-	# plt.show()
+	try:
+		conn = sqlite3.connect(dbname)
+		c=conn.cursor()
+		c.execute('SELECT * FROM Temp')
+		data=c.fetchall()
+		conn.close()
+		
+		data=np.array(data)
+		
+		# for row in data:
+		#         print 'Time: {},\tTemp: {}'.format(row[0],row[1])
+		
+		ax.clear()
+		dates = data[:,0]/sec_per_day
+		ax.plot_date(dates,data[:,1],'o-',dates,data[:,2],'o-')
+		ax.legend(['Mountain View','Sensor'])
+		fig.savefig('out.png')
+
+		f = open('out.png','rb')
+		response = client.put_file('/out.png',f,overwrite=True)
+		print 'uploaded: {}'.format(response)
+		
+		plt.pause(180)
+		# plt.show()
+	except KeyboardInterrupt:
+		raise
+	except:
+		pass
--- WeatherTracker.py
+++ WeatherTracker.py
@@ -9,35 +9,37 @@ import ds18b20 as therm
 sensor = therm.DS18B20()
 
 while True:
-	# Get Mountain View temperature from Wunderground
-	f=urllib2.urlopen('http://api.wunderground.com/api/e260c734250b742d/conditions/q/CA/Mountain_View.json')
-	json_string = f.read()
-	parsed_json = json.loads(json_string)
-	location = 'Mountain View'
 	try:
-		temp_f_WeatherChannel = parsed_json['current_observation']['temp_f']
+		# Get Mountain View temperature from Wunderground
+		f=urllib2.urlopen('http://api.wunderground.com/api/e260c734250b742d/conditions/q/CA/Mountain_View.json')
+		json_string = f.read()
+		parsed_json = json.loads(json_string)
+		location = 'Mountain View'
+		try:
+			temp_f_WeatherChannel = parsed_json['current_observation']['temp_f']
 
-	except:
-		print parsed_json['response']
-		continue
-	print 'Current temperature in {} is {}'.format(location,temp_f_WeatherChannel)
-	f.close()
+		except:
+			print parsed_json['response']
+			continue
+		print 'Current temperature in {} is {}'.format(location,temp_f_WeatherChannel)
+		f.close()
 
-	# Get temperature from sensor
-	temp_f_Sensor = sensor.get_temperature(sensor.DEGREES_F)
-	print 'Sensor temperature is {}'.format(temp_f_Sensor)
-	
-	conn = sqlite3.connect('test.db')
-	c=conn.cursor()
-	try:
-		c.execute('CREATE TABLE Temp(time REAL,temp_f_WeatherChannel REAL,temp_f_Sensor)')
+		# Get temperature from sensor
+		temp_f_Sensor = sensor.get_temperature(sensor.DEGREES_F)
+		print 'Sensor temperature is {}'.format(temp_f_Sensor)
+		
+		conn = sqlite3.connect('test.db')
+		c=conn.cursor()
+		try:
+			c.execute('CREATE TABLE Temp(time REAL,temp_f_WeatherChannel REAL,temp_f_Sensor)')
+		except:
+			pass
+		data=(time.time(),temp_f_WeatherChannel,temp_f_Sensor)
+		c.execute('INSERT INTO Temp VALUES(?,?,?)',data)
+		
+		conn.commit()
+		conn.close()
+
+		time.sleep(180)
 	except:
 		pass
-	data=(time.time(),temp_f_WeatherChannel,temp_f_Sensor)
-	c.execute('INSERT INTO Temp VALUES(?,?,?)',data)
-	
-	conn.commit()
-	conn.close()
-
-	time.sleep(180)
-	
