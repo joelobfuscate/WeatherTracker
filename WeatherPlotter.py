df085e731f1a7c76dbf3a841b0c4c98334f5723a
Joel Frederico <joelfrederico+obfuscate@gmail.com>
2015-03-27 10:19:47 -0700

    Two plots now

--- WeatherPlotter.py
+++ WeatherPlotter.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
+# import pdb
+import argparse
 import sqlite3
-import matplotlib.pyplot as plt
+import matplotlib.figure as mplfig
 import matplotlib.dates as mdates
 import numpy as np
 import dropbox as dbox
@@ -9,6 +11,7 @@ import webbrowser
 import time
 import myhttpd
 from pytz import timezone
+import matplotlib.gridspec as gridspec
 
 # SQL Database
 dbname = 'test.db'
@@ -18,86 +21,123 @@ app_key = 'o4ewwn9y97bnjk4'
 app_secret = 'xt3f58jsdpdvlv4'
 access_type = 'app_folder'
 
-# Try to get access_token from SQL
-conn = sqlite3.connect(dbname)
-c = conn.cursor()
-try:
-	c.execute('SELECT * FROM Dropbox')
-	data=c.fetchall()
-	access_token = data[0][0]
-	print data
-# Get access token from Dropbox
-except IndexError:
-	need_creds = True
-	while need_creds:
-		try:
-			fakesession = dict()
-			flow=dbox.client.DropboxOAuth2Flow(app_key,app_secret,redirect_uri='http://localhost:8080/',session=fakesession,csrf_token_session_key='dropbox-auth-csrf-token')
-			url=flow.start()
-			webbrowser.get('/usr/bin/chromium %s').open(url,new=1)
+def main(days):
+    # Try to get access_token from SQL
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
+    try:
+        c.execute('SELECT * FROM Dropbox')
+        data=c.fetchall()
+        access_token = data[0][0]
+        print data
+    # Get access token from Dropbox
+    except IndexError:
+        need_creds = True
+        while need_creds:
+            try:
+                fakesession = dict()
+                flow=dbox.client.DropboxOAuth2Flow(app_key,app_secret,redirect_uri='http://localhost:8080/',session=fakesession,csrf_token_session_key='dropbox-auth-csrf-token')
+                url=flow.start()
+                webbrowser.get('/usr/bin/chromium %s').open(url,new=1)
+    
+                httpd = myhttpd.ClientRedirectServer(('localhost',8080),myhttpd.ClientRedirectHandler)
+                httpd.handle_request()
+                httpd.server_close()
+    
+                access_token,user_id,url_state=flow.finish(httpd.query_params)
+                print 'access_token is {}'.format(access_token)
+    
+    
+                try:
+                    c.execute('CREATE TABLE Dropbox(access_token TEXT)')
+                except:
+                    pass
+                c.execute('INSERT INTO Dropbox VALUES(?)',(access_token,))
+                conn.commit()
+    
+    
+                need_creds=False
+            except dbox.rest.ErrorResponse:
+                time.sleep(1)
+            except KeyboardInterrupt:
+                raise
+    
+    client = dbox.client.DropboxClient(access_token)
+    print 'linked account: {}'.format(client.account_info())
+    
+    fig = mplfig.Figure(figsize=(8,12))
+    gs = gridspec.GridSpec(2,1)
+    ax = fig.add_subplot(gs[0,0])
+    ax1 = fig.add_subplot(gs[1,0])
+    # sec_per_day = 60.*60.*24.
+    
+    loopy=True
+    while loopy:
+        try:
+            conn = sqlite3.connect(dbname)
+            c=conn.cursor()
+            c.execute('SELECT * FROM Temp')
+            data=c.fetchall()
+            conn.close()
+            
+            data=np.array(data)
+            
+            ax.clear()
+            ax1.clear()
+            dates = data[:,0]
+            datadates = mdates.epoch2num(dates)
 
-			httpd = myhttpd.ClientRedirectServer(('localhost',8080),myhttpd.ClientRedirectHandler)
-			httpd.handle_request()
-			httpd.server_close()
+            mtvtemp = data[:,1]
+            sensortemp = data[:,2]
+            pactime = timezone('US/Pacific')
+            axisfmt = mdates.DateFormatter('%-m/%-d %-H:%M',tz=pactime)
 
-			access_token,user_id,url_state=flow.finish(httpd.query_params)
-			print 'access_token is {}'.format(access_token)
+	    lastweek_bool = (datadates> (mdates.epoch2num(time.time())-7))
+	    plotdates = datadates[lastweek_bool]
+	    mtvtempplot = mtvtemp[lastweek_bool]
+	    sensortempplot = sensortemp[lastweek_bool]
+	    p1=ax.plot_date(plotdates,mtvtempplot,'-')
+	    p2=ax.plot_date(plotdates,sensortempplot,'-')
+	    ax.set_ylabel('Deg. Fahrenheit')
+	    ax.xaxis.set_major_formatter(axisfmt)
 
+            recent_bool = (datadates> (mdates.epoch2num(time.time())-days))
+            plotdates = datadates[recent_bool]
+            mtvtempplot = mtvtemp[recent_bool]
+            sensortempplot = sensortemp[recent_bool]
+            p1=ax1.plot_date(plotdates,mtvtempplot,'-')
+            p2=ax1.plot_date(plotdates,sensortempplot,'-')
+            ax1.set_ylabel('Deg. Fahrenheit')
+            ax1.legend(['Mountain View: {:0.1f}'.format(mtvtemp[-1]),'Sensor: {:0.1f}'.format(sensortemp[-1])],loc=0)
+            ax1.xaxis.set_major_formatter(axisfmt)
+            # ax.set_title('WeatherTracker')
 
-			try:
-				c.execute('CREATE TABLE Dropbox(access_token TEXT)')
-			except:
-				pass
-			c.execute('INSERT INTO Dropbox VALUES(?)',(access_token,))
-			conn.commit()
+	    # ax.autofmt_xdate()
+	    # ax1.autofmt_xdate()
+            # fig.tight_layout()
+            gs.tight_layout(fig)
+            fig.savefig('out.png')
+    
+            f = open('out.png','rb')
+            response = client.put_file('/out.png',f,overwrite=True)
+            f.close()
+            print 'uploaded: {}'.format(response)
+            
+            time.sleep(300)
+            # loopy=False
+            # plt.show()
+        except KeyboardInterrupt:
+            raise
+        except:
+            pass
 
-
-			need_creds=False
-		except dbox.rest.ErrorResponse:
-			time.sleep(1)
-		except KeyboardInterrupt:
-			raise
-
-client = dbox.client.DropboxClient(access_token)
-print 'linked account: {}'.format(client.account_info())
-
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-sec_per_day = 60.*60.*24.
-
-loopy=True
-while loopy:
-	try:
-		conn = sqlite3.connect(dbname)
-		c=conn.cursor()
-		c.execute('SELECT * FROM Temp')
-		data=c.fetchall()
-		conn.close()
-		
-		data=np.array(data)
-		
-		ax.clear()
-		dates = data[:,0]
-		plotdates = mdates.epoch2num(dates)
-		pactime = timezone('US/Pacific')
-		axisfmt = mdates.DateFormatter('%-m/%-d %-H:%M',tz=pactime)
-		p1=ax.plot_date(plotdates,data[:,1],'-')
-		p2=ax.plot_date(plotdates,data[:,2],'-')
-		ax.set_ylabel('Deg. Fahrenheit')
-		ax.legend(['Mountain View','Sensor'],loc=4,ncol=2,bbox_to_anchor=(1,1.02),borderaxespad=0)
-		ax.xaxis.set_major_formatter(axisfmt)
-		# ax.set_title('WeatherTracker')
-		fig.autofmt_xdate()
-		# fig.tight_layout()
-		fig.savefig('out.png')
-
-		f = open('out.png','rb')
-		response = client.put_file('/out.png',f,overwrite=True)
-		f.close()
-		print 'uploaded: {}'.format(response)
-		
-		time.sleep(180)
-		# loopy=False
-		# plt.show()
-	except KeyboardInterrupt:
-		raise
+if __name__ == '__main__':
+    parser=argparse.ArgumentParser(description='Loads and runs a gui to analyze saved spectrometer data.')
+    parser.add_argument('-v','--verbose',action='store_true',
+        help='enable verbose mode')
+    parser.add_argument('-d','--days',type=float,
+        help='image number')
+    
+    arg=parser.parse_args()
+    
+    main(arg.days)
--- WeatherTracker.py
+++ WeatherTracker.py
@@ -4,44 +4,45 @@ import json
 import urllib2
 import sqlite3
 import time
-import ds18b20 as therm
+import w1thermsensor as therm
 
-sensor = therm.DS18B20()
+sensor = therm.W1ThermSensor()
 
 while True:
-	try:
-		# Get Mountain View temperature from Wunderground
-		f=urllib2.urlopen('http://api.wunderground.com/api/e260c734250b742d/conditions/q/CA/Mountain_View.json')
-		json_string = f.read()
-		parsed_json = json.loads(json_string)
-		location = 'Mountain View'
-		try:
-			temp_f_WeatherChannel = parsed_json['current_observation']['temp_f']
+    try:
+        # Get Mountain View temperature from Wunderground
+        f=urllib2.urlopen('http://api.wunderground.com/api/e260c734250b742d/conditions/q/CA/Mountain_View.json')
+        json_string = f.read()
+        parsed_json = json.loads(json_string)
+        location = 'Mountain View'
+        try:
+            temp_f_WeatherChannel = parsed_json['current_observation']['temp_f']
 
-		except:
-			print parsed_json['response']
-			continue
-		print 'Current temperature in {} is {}'.format(location,temp_f_WeatherChannel)
-		f.close()
+        except:
+            print parsed_json['response']
+            continue
+        print 'Current temperature in {} is {}'.format(location,temp_f_WeatherChannel)
+        f.close()
 
-		# Get temperature from sensor
-		temp_f_Sensor = sensor.get_temperature(sensor.DEGREES_F)
-		print 'Sensor temperature is {}'.format(temp_f_Sensor)
-		
-		conn = sqlite3.connect('test.db')
-		c=conn.cursor()
-		try:
-			c.execute('CREATE TABLE Temp(time REAL,temp_f_WeatherChannel REAL,temp_f_Sensor REAL)')
-		except:
-			pass
-		data=(time.time(),temp_f_WeatherChannel,temp_f_Sensor)
-		c.execute('INSERT INTO Temp VALUES(?,?,?)',data)
-		
-		conn.commit()
-		conn.close()
+        # Get temperature from sensor
+        temp_f_Sensor = sensor.get_temperature(therm.W1ThermSensor.DEGREES_F)
+        print 'Sensor temperature is {}'.format(temp_f_Sensor)
+        
+        conn = sqlite3.connect('test.db')
+        c=conn.cursor()
+        try:
+            c.execute('CREATE TABLE Temp(time REAL,temp_f_WeatherChannel REAL,temp_f_Sensor REAL)')
+        except:
+            pass
+        data=(time.time(),temp_f_WeatherChannel,temp_f_Sensor)
+        c.execute('INSERT INTO Temp VALUES(?,?,?)',data)
+        
+        conn.commit()
+        conn.close()
 
-		time.sleep(180)
-	except KeyboardInterrupt:
-		raise
-	except:
-		pass
+    except KeyboardInterrupt:
+        raise
+    except:
+        pass
+
+    time.sleep(300)
